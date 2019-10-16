63e8071cd37f8ef6c898bb30fc89f0497a13e9b4
Masnoon Junaid <masnoon20@gmail.com>
2019-10-16 17:56:45 +0530

    test resources for gallary page

---
+++ template/js/app.js
@@ -0,0 +1,51 @@
+const collection = [
+  {
+    id : 1,
+    img : '../images/img1.jpg',
+    title: 'Whatever'
+  },
+  {
+    id : 2,
+    img: '../images/img2.jpg',
+    title: 'Whatever'
+  },
+  {
+    id : 3,
+    img: '../images/img3.jpg',
+    title: 'Whatever'
+  },
+  {
+    id : 4,
+    img: '../images/img4.jpg',
+    title: 'Whatever'
+  },
+  {
+    id : 5,
+    img: '../images/img5.jpg',
+    title: 'Whatever'
+  },
+  {
+    id : 6,
+    img: '../images/img6.jpg',
+    title: 'Whatever'
+  },
+  {
+    id : 7,
+    img: '../images/img7.jpg',
+    title: 'Whatever'
+  },
+  {
+    id : 8,
+    img: '../images/img8.jpg',
+    title: 'Whatever'
+  },
+
+]
+
+const gallary = document.getElementById('gallary')
+(init = () => {
+  for(let = 0; i < collection.length; i++){
+    let element = document.createElement(`<li><img src=""></img></li>`)
+    
+  }
+})()
